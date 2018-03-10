5ba6e4c62155f53b1c577bcb9d81ae3d0adac7bc
Hiroki11x <hiroki11x@gmail.com>
2018-03-10 14:00:13 +0900

    complete parameter server

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 sender_port = 8004
 receiver_port = 8008
+
+interction = 0
+
+sender_ip = "127.0.0.1"
+receiver_ip = "127.0.0.1"
--- parameter_server.py
+++ parameter_server.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.random import *
 
 # 更新時間管理など
 import datetime
@@ -23,53 +24,98 @@ from pythonosc import udp_client
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+import threading
+
 # Ctrl+Cで終了時の処理
 def handler(signal, frame):
         print('Exit with Ctrl+C / sys.exit(0) ')
         sys.exit(0)
 
+# OSCのSender初期化
 def init_osc_sender():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sender_ip", default="127.0.0.1", help="The ip of th OSC Sender")
+    parser.add_argument("--sender_ip", default=config.sender_ip, help="The ip of th OSC Sender")
     parser.add_argument("--sender_port", type=int, default=config.sender_port, help="The port the OSC sender is listening on")
     args = parser.parse_args()
     osc_client = udp_client.UDPClient(args.sender_ip, args.sender_port)
 
     # 設定のログ出し
-    print("sender_ip:127.0.0.1, sender_port:" + str(config.sender_port) + ", address:/data")
+    print("[Sender] sender_ip:{}, sender_port:{}, address:/data".format(args.sender_ip, args.sender_port))
     return osc_client
 
-# Parameterの取得
+# OSCのReceiver初期化
+def receiver_thread():
+    init_osc_receiver()
+
+# OSC Receiverの初期化
+def init_osc_receiver():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--receiver_ip",default=config.receiver_ip, help="The ip to listen on")
+    parser.add_argument("--receiver_port",type=int, default=config.receiver_port, help="The port the OSC Receiver to listen on")
+    args = parser.parse_args()
+
+    _dispatcher = dispatcher.Dispatcher()
+    _dispatcher.map("/data", print_handler)
+
+    server = osc_server.ThreadingOSCUDPServer((args.receiver_ip, args.receiver_port), _dispatcher)
+    print("[Receiver] Receiving on {}".format(server.server_address))
+
+    server.serve_forever()
+
+# Receive時の処理 (data/ 以降に一つのデータの際(引数一つ)であればok)
+def print_handler(_data_path, arg):
+    print("receive interaction : {0}".format((arg)))
+    config.interction = arg
+
+# SendするParameterの取得
 def get_param(arg):
-    print("type {}:".format(arg))
-    input_arg = input()
+    input_arg = randint(10,100)
     return input_arg
 
-def task(osc_client):
+# ParameterをBroadCastする
+def broadcast_parameter(osc_client):
+    msg = osc_message_builder.OscMessageBuilder(address="/data")
 
-  input_arg_x = get_param("X")
-  input_arg_y = get_param("Y")
-  input_arg_z = get_param("Z")
-  input_arg_interction = get_param("interction")
+    # 適当なX,Y,Zを取得
+    # TODO @haradama
+    input_arg_x = get_param("X")
+    input_arg_y = get_param("Y")
+    input_arg_z = get_param("Z")
 
-  msg = osc_message_builder.OscMessageBuilder(address="/data")
+    # interactionの値を取得してるやつをServe
+    input_arg_interction = config.interction
 
-  msg.add_arg(input_arg_x)
-  msg.add_arg(input_arg_y)
-  msg.add_arg(input_arg_z)
-  msg.add_arg(input_arg_interction)
+    msg.add_arg(input_arg_x)
+    msg.add_arg(input_arg_y)
+    msg.add_arg(input_arg_z)
+    msg.add_arg(input_arg_interction)
 
-  msg = msg.build()
-  osc_client.send(msg)
+    print("send x : {0}".format(input_arg_x))
+    print("send y : {0}".format(input_arg_y))
+    print("send z : {0}".format(input_arg_z))
+    print("send interction : {0}".format(input_arg_interction))
+
+    msg = msg.build()
+    osc_client.send(msg)
+
+def main_thread():
+
+    # Sender OSC周りの初期化
+    osc_client_sender = init_osc_sender()
+
+    # SenderでParameterをBroadCast
+    while True:
+        broadcast_parameter(osc_client_sender)
+        time.sleep(10)
 
 if __name__ == "__main__":
 
-  # Ctrl+C終了時のイベント検知
-  signal.signal(signal.SIGINT, handler)
+    # Ctrl+C終了時のイベント検知
+    signal.signal(signal.SIGINT, handler)
 
-  # OSC周りの初期化
-  osc_client_sender = init_osc_sender()
+    # ReceiverのThreadを別Threadで実行
+    receive_thread = threading.Thread(target=receiver_thread,name="dual_loop")
+    receive_thread.start()
 
-  while True:
-    task(osc_client_sender)
-    time.sleep(2)
+    # BroadCastのloop
+    main_thread()
