beb7b94a3eb9a94c0e1a3c84b443d805a08b64ca
Hiroki11x <hiroki11x@gmail.com>
2018-03-10 21:42:36 +0900

    parameter tuning

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
-sender_port = 8004
-receiver_port = 8008
+sender_port = 8008
+receiver_port = 8004
 
 interction = 0
 
-sender_ip = "127.0.0.1"
+sender_ip = "192.168.2.106"
 receiver_ip = "127.0.0.1"
+
+pattern_num = 100
+sample_num = 10000
+
+x_max = 1200
+y_max = 2700
