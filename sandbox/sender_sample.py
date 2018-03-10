540bd74122d49ff49d3b76ba8bb10f9de70b27be
Hiroki11x <hiroki11x@gmail.com>
2018-03-10 18:00:29 +0900

    create time-series-data generator

--- sandbox/sender_sample.py
+++ sandbox/sender_sample.py
@@ -22,24 +22,33 @@ def handler(signal, frame):
 # OSCのSender初期化
 def init_osc_sender():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sender_ip", default="127.0.0.1", help="The ip of th OSC Sender")
+    parser.add_argument("--sender_ip", default=config.sender_ip, help="The ip of th OSC Sender")
     parser.add_argument("--sender_port", type=int, default=config.sender_port, help="The port the OSC sender is listening on")
     args = parser.parse_args()
     osc_client = udp_client.UDPClient(args.sender_ip, args.sender_port)
 
     # 設定のログ出し
-    print("sender_ip:127.0.0.1, sender_port:" + str(config.sender_port) + ", address:/data")
+    print("[Sender] sender_ip:{}, sender_port:{}, address:/data".format(args.sender_ip, args.sender_port))
     return osc_client
 
 # Parameterの取得とmsgに突っ込む
 def get_param(msg,arg):
     print("type {}:".format(arg))
     input_arg = input()
-    msg.add_arg(input_arg)
+    msg.add_arg(int(input_arg))
     return input_arg
 
 def task(osc_client):
   msg = osc_message_builder.OscMessageBuilder(address="/data")
+
+  # 更新時間を送信
+  update_time = str(time.asctime().split(" ")[3])
+  msg.add_arg(update_time)
+  print("update_time: {}".format(update_time))
+
+  get_param(msg,"X")
+  get_param(msg,"Y")
+  get_param(msg,"Z")
   get_param(msg,"interaction")
 
   msg = msg.build()
