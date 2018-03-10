98ac4f04448e78068488e9371260fdbd4443e71d
Hiroki11x <hiroki11x@gmail.com>
2018-03-11 02:18:25 +0900

    implement multiple osc sender

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
-sender_port = 8012
 receiver_port = 8008
-
 interaction = 0
-
-sender_ip = "192.168.2.106"
-# sender_ip = "192.168.2.106"
-
 receiver_ip = "127.0.0.1"
+
+# projector
+projector_sender_ip = "192.168.2.14"
+projector_sender_port = 8011
+
+# PureData
+pd_sender_ip = "192.168.2.12"
+pd_sender_port = 8012
+
+# roomba
+roomba_sender_ip = "192.168.2.13"
+roomba_sender_port = 8013
 
 # 多いほど複雑な動きが生成
 pattern_num = 100
---
+++ demo/config.py
@@ -0,0 +1,3 @@
+sender_port = 8008
+sender_ip="127.0.0.1"
+receiver_port = 8004
