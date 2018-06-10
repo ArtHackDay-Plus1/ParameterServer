ef36d6acd692c156542cd859a6adfe6734870fcb
Hiroki11x <hiroki11x@gmail.com>
2018-06-10 16:42:02 +0900

    refect interaction methodology

--- parameter_server.py
+++ parameter_server.py
@@ -79,9 +79,9 @@ def generate_perlin_noise():
     return x_list,y_list;
 
 # Parameterの取得とmsgに突っ込む
-def get_param(msg,arg):
-    input_arg = randint(0,100)
-    msg.add_arg(int(input_arg))
+# def get_param(msg,arg):
+#     input_arg = randint(0,100)
+#     msg.add_arg(int(input_arg))
 
 def broadcast_parameter(osc_client, x, y, z, interaction):
     try:
@@ -109,44 +109,74 @@ def get_distance(x1, y1, x2, y2):
     d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return d
 
-# def calculate_interaction():
-#     interaction_value = received_data.
-#     return interaction_value
+#検知したInteractionの0~2700の位置をもとに、ルンバの逃げる位置を決定
+def get_farthest_xy(target_x):
+    if target_x < 1350:
+        f_x = 0 + frame_margin
+    else:
+        f_x = 2700 - frame_margin
+    f_y = 0 + frame_margin # 一番壁側
+    return f_x,f_y
+
+# 任意の点(x,y)最近傍の 生成済み経路の点のindexを取得
+def calculate_nearest_index(x, y):
+    index = -1
+    min_distance = 10000
+    for i in range(config.sample_num):
+        if get_distance(x, y, x_list[i], y_list[i]) < min_distance :
+            min_distance = get_distance(x, y, x_list[i], y_list[i])
+            index = i
+    return nearest_index
 
 def main_thread():
 
     # OSC周りの初期化
     macmini_osc_client_sender = init_osc_sender(config.macmini_sender_ip,config.macmini_sender_port)
-
     pd_osc_client_sender = init_osc_sender(config.pd_sender_ip,config.pd_sender_port)
-
     roomba_osc_client_sender = init_osc_sender(config.roomba_sender_ip,config.roomba_sender_port)
 
+    # 更新パラメータ
     index = 0
+
     while True:
         x = x_list[index]
         y = y_list[index]
+        # zに何かしらインタラクション入れたい
         z = randint(0,config.z_max)
 
         # 人が多い場合はinteraction度合いをあげる
         interaction = received_data.num_of_people
-        broadcast_parameter(macmini_osc_client_sender, x, y, z, interaction)
-        broadcast_parameter(pd_osc_client_sender, x, y, z, interaction)
-        broadcast_parameter(roomba_osc_client_sender, x, y, z, interaction)
 
-        # 人に近い場合は素早く動く
         # 一番展示に近い人のy方向の値をmapで取得
         target_x = received_data.nearest_x
+
         # 一番展示に近い人の展示までの距離をmapで取得
         target_y = received_data.nearest_depth
 
-        if(get_distance(x, y, target_x, -target_y) < config.prohibited_area_radius):
+        # この時Interactionの検知なし(誰も見てないだろうから更新もほぼしない)
+        if(target_x<0 or target_y<0):
+            # 普段の時、Interactionは検知しているが、ある程度遠い時
+            time.sleep(5)
+
+        # Interaction検知してる時、roombaと人がある程度近いと逃げる
+        elif(get_distance(x, y, target_x, target_y) < config.prohibited_area_radius):
             print("NEAR")
-            if(interaction >= config.interaction_threshold):
-                
-                time.sleep(0.001)
-            else:
-                time.sleep(0.05)
+            # 以降しばらくは特定の位置情報だけ送ってそこに向かうようにさせる
+            # -> そのためにIndex値を固定 その特定座標と近いIndexを求める
+            f_x, f_y = get_farthest_xy(target_x)
+            index = calculate_nearest_index(f_x, f_y)
+            # 送る座標も固定
+            x, y = f_x, f_y
+            # Swithのためのkeyみたいなboolean変数を一個用意
+            # しばらく更新しないことでここに行って止まる気がする
+            time.sleep(10)
+        else:
+            # 普段の時、Interactionは検知しているが、ある程度遠い時
+            time.sleep(0.05)
+
+        broadcast_parameter(macmini_osc_client_sender, x, y, z, interaction)
+        broadcast_parameter(pd_osc_client_sender, x, y, z, interaction)
+        broadcast_parameter(roomba_osc_client_sender, x, y, z, interaction)
 
         # Interaction の 0/1スイッチのみでの制御 for DEMO
         # if(interaction < 0):
